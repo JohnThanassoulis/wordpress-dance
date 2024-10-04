f1ead8ac10556b1d24d28160c52cf643260ddad5
JohnThanassoulis <johnthanassoulis@hotmail.com>
2024-10-04 17:12:45 +0300

    Test Lightbox Change

---
+++ api/lightbox-switcher.js
@@ -0,0 +1,22 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+export default async function handler(req, res) {
+    try {
+        // Get the absolute path to the gallery file
+        const galleryFilePath = path.resolve('./public/gallery/index.html');
+
+        // Read the gallery file content
+        const galleryContent = await fs.readFile(galleryFilePath, 'utf-8');
+
+        // Replace the attributes
+        const modifiedContent = galleryContent.replace(/data-elementor-open-lightbox/g, 'data-fslightbox');
+
+        // Send the modified content as the response
+        res.setHeader('Content-Type', 'text/html');
+        res.status(200).send(modifiedContent);
+    } catch (error) {
+        console.error("Error occurred:", error); // Log the error to the console for debugging
+        res.status(500).json({ message: "Error reading or modifying gallery file.", error: error.toString() });
+    }
+}
